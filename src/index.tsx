a1c2a6543533fa2db6f055bc5f60551e6aaf7f91
Kristo Isberg <kristo.isberg@fleep.io>
2022-05-22 18:14:03 +0300

    Fix CSS not being loaded

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "core-js/stable";
 import { createRoot } from "react-dom/client";
 
 import App from "./app";
+import "./style.css";
 
 const container = document.getElementById("root");
 if (container !== null) {
