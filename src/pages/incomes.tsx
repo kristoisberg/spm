059ceb7710bbc00b5d9ad5ebfbfd52b7a1dbb795
Kristo Isberg <kristo.isberg@fleep.io>
2022-05-22 17:49:34 +0300

    Implement editing incomes

--- src/pages/incomes.tsx
+++ src/pages/incomes.tsx
@@ -1,34 +1,108 @@
 import { CloseOutlined, DeleteFilled, EditFilled, PlusOutlined, SaveFilled } from "@ant-design/icons";
-import { Button, Popconfirm, Table, Typography } from "antd";
+import { Button, Form, Input, InputNumber, Popconfirm, Table, Typography } from "antd";
 import { ColumnType } from "antd/lib/table";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Income from "../types/income";
 
 const Incomes = () => {
+  const [form] = Form.useForm();
   const [incomes, setIncomes] = useState<Income[]>([
     { id: uuidv4(), name: "Salary", amount: 1234, date: "2022-05-03" },
     { id: uuidv4(), name: "Scholarship", amount: 300, date: "2022-05-08" },
   ]);
   const [editingRow, setEditingRow] = useState<Income | null>(null);
+  const [isEditingAddedRow, setEditingAddedRow] = useState(false);
 
   const columns: ColumnType<Income>[] = [
-    { title: "Name", dataIndex: "name", key: "name", align: "center" },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (name, income) => {
+        if (editingRow === null || editingRow.id !== income.id) {
+          return name;
+        }
+        return (
+          <Form.Item
+            style={{
+              margin: 0,
+            }}
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: `Name is required.`,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        );
+      },
+      align: "center",
+    },
     {
       title: "Amount",
       dataIndex: "amount",
       key: "amount",
-      render: (amount) => `$${Number(amount).toFixed(2)}`,
+      render: (amount, income) => {
+        if (editingRow === null || editingRow.id !== income.id) {
+          return `$${Number(amount).toFixed(2)}`;
+        }
+        return (
+          <Form.Item
+            style={{
+              margin: 0,
+            }}
+            name="amount"
+            rules={[
+              {
+                required: true,
+                message: `Amount is required.`,
+              },
+            ]}
+          >
+            <InputNumber prefix="$" />
+          </Form.Item>
+        );
+      },
       align: "center",
     },
-    { title: "Date", dataIndex: "date", key: "date", align: "center" },
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+      render: (date, income) => {
+        if (editingRow === null || editingRow.id !== income.id) {
+          return date;
+        }
+        return (
+          <Form.Item
+            style={{
+              margin: 0,
+            }}
+            name="date"
+            rules={[
+              {
+                required: true,
+                message: `Date is required.`,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        );
+      },
+      align: "center",
+    },
     {
       title: "Operations",
       align: "right",
       render: (_, income) =>
         editingRow === null ? (
           <>
-            <Button icon={<EditFilled />} type="primary" onClick={() => setEditingRow(income)} />{" "}
+            <Button icon={<EditFilled />} type="primary" onClick={() => onStartEditingRow(income)} />{" "}
             <Popconfirm title="Sure to delete?" onConfirm={() => onDeleteRow(income.id)}>
               <Button icon={<DeleteFilled />} type="primary" danger />
             </Popconfirm>
@@ -36,7 +110,7 @@ const Incomes = () => {
         ) : editingRow.id === income.id ? (
           <>
             <Button icon={<SaveFilled />} type="primary" onClick={() => onEditRow(editingRow)} />{" "}
-            <Button icon={<CloseOutlined />} type="primary" danger onClick={() => setEditingRow(null)} />
+            <Button icon={<CloseOutlined />} type="primary" danger onClick={() => onCancelEditingRow()} />
           </>
         ) : (
           <></>
@@ -44,8 +118,29 @@ const Incomes = () => {
     },
   ];
 
-  const onEditRow = (row: Income) => {
-    setIncomes((incomes) => incomes.map((income) => (income.id === row.id ? row : income)));
+  const onAddRow = () => {
+    const newRow: Income = { id: uuidv4(), name: "", amount: 0, date: "" };
+    setIncomes((incomes) => [...incomes, newRow]);
+    setEditingRow(newRow);
+    setEditingAddedRow(true);
+  };
+
+  const onStartEditingRow = (row: Income) => {
+    form.setFieldsValue(row);
+    setEditingRow(row);
+  };
+
+  const onEditRow = async (row: Income) => {
+    const newRow = await form.validateFields();
+    setIncomes((incomes) => incomes.map((income) => (income.id === row.id ? newRow : income)));
+    setEditingRow(null);
+  };
+
+  const onCancelEditingRow = () => {
+    if (isEditingAddedRow && editingRow) {
+      setEditingAddedRow(false);
+      onDeleteRow(editingRow.id);
+    }
     setEditingRow(null);
   };
 
@@ -56,10 +151,24 @@ const Incomes = () => {
   return (
     <>
       <Typography.Title>Incomes</Typography.Title>
-      <Button icon={<PlusOutlined />} type="primary" style={{ marginBottom: "1em", float: "right" }}>
+      <Button
+        icon={<PlusOutlined />}
+        type="primary"
+        style={{ marginBottom: "1em", float: "right" }}
+        onClick={() => onAddRow()}
+      >
         New income
       </Button>
-      <Table<Income> columns={columns} dataSource={incomes} rowKey="id" />
+      <Form form={form} component={false}>
+        <Table<Income>
+          columns={columns}
+          dataSource={incomes}
+          rowKey="id"
+          pagination={{
+            onChange: onCancelEditingRow,
+          }}
+        />
+      </Form>
     </>
   );
 };
